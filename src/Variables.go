c00377da9ac82ccd70379cab5e5e0a42292079eb
Maxwell Wang <maxwang@udel.edu>
2022-06-20 12:04:13 -0400

    basic variables ranging from strings to floats

---
+++ src/Variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "Original"
+	fmt.Printf(a + "\n")
+
+	var b, c int = 9, 10
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+}
