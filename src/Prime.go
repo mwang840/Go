9b1d08238bb79241dff582846485b7a903c1d6e9
Maxwell Wang <maxwang@udel.edu>
2022-08-27 11:12:02 -0400

    feat optimization

--- src/Prime.go
+++ src/Prime.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Prime(n int) bool {
 	if n <= 1 {
 		return false
 	}
-	for i := 2; i < n/2; i++ {
+	for i := 2; i <= int(math.Floor(math.Sqrt(float64(n)))); i++ {
 		if n%i == 0 {
 			return false
 		}
@@ -17,6 +18,10 @@ func Prime(n int) bool {
 }
 
 func main() {
-	fmt.Print(Prime(3))
+	fmt.Println(Prime(3))
 	fmt.Println(Prime(4))
+	fmt.Println(Prime(49))
+	fmt.Println(Prime(77))
+	fmt.Println(Prime(103))
+	fmt.Println(Prime(361))
 }
